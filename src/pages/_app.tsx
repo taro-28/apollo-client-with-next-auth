721c8ed14776a81db1b9c06e4568a4561f302424
taro-28 <tarotaro0208@gmail.com>
2024-01-20 17:52:14 +0900

    Add Apollo Client and GraphQL dependencies, implement ApolloProvider in _app.tsx, and useQuery in index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
 import "@/styles/globals.css";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
 import type { AppProps } from "next/app";
 
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
 export default function Home() {
-  return <main />;
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  return (
+    <main>
+      {data.locations.map(({ id, name, description, photo }) => (
+        <div key={id}>
+          <h3>{name}</h3>
+          <img
+            width="400"
+            height="250"
+            alt="location-reference"
+            src={`${photo}`}
+          />
+          <br />
+          <b>About this location:</b>
+          <p>{description}</p>
+          <br />
+        </div>
+      ))}
+    </main>
+  );
 }
